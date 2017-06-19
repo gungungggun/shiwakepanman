3dd85ab9d7fe4e1f8f2a2a87758ed4e30a16466b
Gun <gungungggun@gmail.com>
2017-06-20 00:14:13 +0900

    設定ファイルに＋キャラ追加

---
+++ vue/src/constants/char.js
@@ -0,0 +1,158 @@
+module.exports = {
+  charcter: {
+    master: [
+      /*{
+        name: "anpanman",
+        color: "#f10000"
+      },
+      {
+        name: "baikinman",
+        color: "#5e0094"
+      },
+      {
+        name: "dokinchan",
+        color: "#ff6600"
+      },
+      {
+        name: "karepanman",
+        color: "#9c6d4b"
+      },
+      {
+        name: "shokupanman",
+        color: "#ffffff"
+      },
+      {
+        name: "jamojisan",
+        color: "#dcfffc"
+      },
+      {
+        name: "batako",
+        color: "#005998"
+      },
+      {
+        name: "chizu",
+        color: "#ffca35"
+      },
+      {
+        name: "kokinchan",
+        color: "#00a0e0"
+      },
+      {
+        name: "horaman",
+        color: "#8f5396"
+      },
+      {
+        name: "meronpanna",
+        color: "#d3ffb2"
+      },
+      {
+        name: "rorupanna",
+        color: "#1d0838"
+      },
+      {
+        name: "kurimupanda",
+        color: "#ffcf78"
+      },
+      {
+        name: "akachanman",
+        color: "#ff5be3"
+      },
+      {
+        name: "komusubiman",
+        color: "#ffeeee"
+      },
+      {
+        name: "omusubiman",
+        color: "#fffefe"
+      },
+      {
+        name: "kabirunrun",
+        color: "#b5a1bd"
+      },
+      {
+        name: "dadandan",
+        color: "#493f4a"
+      },
+      {
+        name: "hanbagakid",
+        color: "#f1a548"
+      },
+      {
+        name: "katsudonman",
+        color: "#c4ffd0"
+      },
+      {
+        name: "tendonman",
+        color: "#ccfdff"
+      },
+      {
+        name: "kamameshidon",
+        color: "#525252"
+      },
+      {
+        name: "slman",
+        color: "#d63030"
+      },
+      {
+        name: "poppochan",
+        color: "#ffa9d1"
+      },
+      {
+        name: "baikinsennin",
+        color: "#975ba2"
+      },
+      {
+        name: "naganegiman",
+        color: "#42bf4c"
+      },
+      {
+        name: "datemakiman",
+        color: "#444444"
+      },
+      {
+        name: "shiratamasan",
+        color: "#ffffff"
+      },
+      {
+        name: "bikubikuchan",
+        color: "#ceecf7"
+      },
+      {
+        name: "burikiddo",
+        color: "#4274bf"
+      },
+      {
+        name: "kabao",
+        color: "#a27100"
+      },
+      {
+        name: "dorianoujo",
+        color: "#f3b11c"
+      },
+      {
+        name: "baikichikun",
+        color: "#975ba2"
+      },*/
+      {
+        name: "chibizokun",
+        color: "#ff00a5"
+      },
+      {
+        name: "chawanmushimaro",
+        color: "#de83d7"
+      },
+      {
+        name: "ringobouya",
+        color: "#f39a9a"
+      },
+      {
+        name: "kotetsucchan",
+        color: "#484848"
+      },
+      {
+        name: "kemuriinu",
+        color: "#ffeba0"
+      }
+    ]
+  }
+}
